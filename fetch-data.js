67ef6adb84433b3865e9da65e677ae8692a12f82
fitsum <fitsumv7@gmail.com>
2024-10-15 02:34:58 +0300

    add fetch files

---
+++ fetch-data.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  async function fetchUserData() {
+    const apiUrl = "https://jsonplaceholder.typicode.com/users";
+    const dataContainer = document.getElementById('api-data');
+  
+    try {
+      const response = await fetch (apiUrl);
+      const users = await response.json();
+      dataContainer.innerHTML = '';
+      const userList = document.createElement('ul');
+
+      users.forEach(user => {
+        const list = document.createElement('li');
+        list.textContent = user.name;
+        userList.appendChild(list);
+      } )
+      
+      dataContainer.appendChild(userList);
+  
+    }
+    catch (error) {
+
+      dataContainer.textContent = 'Failed to load user data.';
+
+    }
+  }
+  fetchUserData();
+}
+  
+  
+
+)
+
